9d35f58389dd7c2e3ff3a5347a3bb7a6044d76d2
BJeab <BJeabby@hotmail.fr>
2021-03-23 22:54:54 +0100

    :recycle: changment tree file and confif

--- client/src/main.js
+++ client/src/main.js
@@ -3,6 +3,9 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
-import './icons.js'
+import './plugins/font-awesome'
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App)
+    .use(store)
+    .use(router)
+    .mount('#app')
--- client/src/plugins/font-awesome.js
+++ client/src/plugins/font-awesome.js
@@ -1,5 +1,10 @@
 import { library, dom } from '@fortawesome/fontawesome-svg-core'
+//import { faPowerOff } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+//library.add(faPowerOff)
 import { faFacebook, faTwitter, faLinkedin } from '@fortawesome/free-brands-svg-icons'
-
 library.add(faFacebook, faTwitter, faLinkedin)
 dom.watch()
+export {
+  FontAwesomeIcon
+}
