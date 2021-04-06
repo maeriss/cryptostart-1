acd4c99ebc8a405beef52b61fdce2f6ba5e38997
maeriss <71259448+maeriss@users.noreply.github.com>
2021-04-06 23:57:13 +0200

    responsive currencies page

--- client/src/views/Currencies.vue
+++ client/src/views/Currencies.vue
@@ -1,59 +1,47 @@
 <template>
-  <div>
-    <h2>This is the page for the currencies list</h2>
-    <div class="layout row">
+  <div class="application--wrap">
+    <div class="layout mt-2 row">
       <div class="flex text-xs-center xs12">
         <div class="card" style="height: auto;">
-          <div class="layout">
-            <div class="flex">
-              <div class="card card--flat" style="height: auto;">
-                <div class="container fluid grid-list-md">
-                  <div class="layout row wrap">
-                    <div class="flex xs12 sm6 md4 lg4">
-                      <router-link to="/currencies/monero" class="ma-2 pa-3 pt-0 card card--hover" style="height: auto;">
-                        <div class="card__text">
-                          <h5 class="headline headings">Monero</h5>
-                          <span class="grey--text headings">XMR</span>
-                        </div>
-                        <div class="card__media" style="height: 216px;">
-                          <div class="card__media__background" style="background: rgba(0, 0, 0, 0) no-repeat scroll center center / contain;">
-                            <img src="../assets/monero.png" alt="image btc">
-                          </div>
-                          <div class="card__media__content"></div>
-                        </div>
-                      </router-link>
-                    </div>
-                    <div class="flex xs12 sm6 md4 lg4">
-                      <router-link to="/nothing" class="ma-2 pa-3 pt-0 card card--hover" style="height: auto;">
-                        <div class="card__text">
-                          <h5 class="headline headings">Bitcoin</h5>
-                          <span class="grey--text headings">BTC</span>
-                        </div>
-                        <div class="card__media" style="height: 216px;">
-                          <div class="card__media__background" style="background: rgba(0, 0, 0, 0) no-repeat scroll center center / contain;">
-                            <img src="../assets/btc.jpg" alt="image btc">
-                          </div>
-                          <div class="card__media__content"></div>
-                        </div>
-                      </router-link>
-                    </div>
-                    <div class="flex xs12 sm6 md4 lg4">
-                      <router-link to="/nothing" class="ma-2 pa-3 pt-0 card card--hover" style="height: auto;">
-                        <div class="card__text">
-                          <h5 class="headline headings">Ethereum</h5>
-                          <span class="grey--text headings">ETH</span>
-                        </div>
-                        <div class="card__media" style="height: 216px;">
-                          <div class="card__media__background" style="background: rgba(0, 0, 0, 0) no-repeat scroll center center / contain;">
-                            <img src="../assets/eth.jpg" alt="image btc">
-                          </div>
-                          <div class="card__media__content"></div>
-                        </div>
-                      </router-link>
-                    </div>
+          <div class="layout row wrap mb-3">
+            <div class="flex xs12 sm6 md4 lg4 xl4">
+              <router-link to="/currencies/monero" class="box card card--hover" style="height: auto;">
+                <div class="card__text">
+                  <h5 class="headline ma-0">Monero</h5>
+                  <span class="grey--text ma-0">XMR</span>
+                </div>
+                <div class="card__media" style="height: 216px;">
+                  <div class="card__media__background" style="background: rgba(0, 0, 0, 0) no-repeat scroll center center / contain;">
+                    <img src="../assets/monero.png" alt="image btc">
                   </div>
                 </div>
-              </div>
+              </router-link>
+            </div>
+            <div class="flex xs12 sm6 md4 lg4 xl4">
+              <router-link to="/currencies/bitcoin" class="box card card--hover" style="height: auto;">
+                <div class="card__text">
+                  <h5 class="headline ma-0">Bitcoin</h5>
+                  <span class="grey--text ma-0">BTC</span>
+                </div>
+                <div class="card__media" style="height: 216px;">
+                  <div class="card__media__background" style="background: rgba(0, 0, 0, 0) no-repeat scroll center center / contain;">
+                    <img src="../assets/btc.jpg" alt="image btc">
+                  </div>
+                </div>
+              </router-link>
+            </div>
+            <div class="flex xs12 sm6 md4 lg4 xl4">
+              <router-link to="/currencies/ether" class="box card card--hover" style="height: auto;">
+                <div class="card__text">
+                  <h5 class="headline ma-0">Ether</h5>
+                  <span class="grey--text ma-0">ETH</span>
+                </div>
+                <div class="card__media" style="height: 216px;">
+                  <div class="card__media__background" style="background: rgba(0, 0, 0, 0) no-repeat scroll center center / contain;">
+                    <img src="../assets/eth.jpg" alt="image btc">
+                  </div>
+                </div>
+              </router-link>
             </div>
           </div>
         </div>
@@ -64,32 +52,21 @@
 
 <script>
   export default {
-
   }
 </script>
 
 <style lang="scss" scoped>
 
-.layout.row {
-    -ms-flex-direction: row;
-    flex-direction: row;
-}
-
-.flex.xs12 {
-    -ms-flex-preferred-size: 100%;
-    flex-basis: 100%;
-    -ms-flex-positive: 0;
-    flex-grow: 0;
+.application--wrap {
+    -ms-flex: 1 1 auto;
+    flex: 1 1 auto;
+    -webkit-backface-visibility: hidden;
+    backface-visibility: hidden;
+    -ms-flex-direction: column;
+    flex-direction: column;
+    min-height: 80vh;
     max-width: 100%;
-}
-
-.card {
-    display: block;
-    border-radius: 2px;
-    min-width: 0;
     position: relative;
-    text-decoration: none;
-    box-shadow: 0 2px 1px -1px rgba(0,0,0,.2),0 1px 1px 0 rgba(0,0,0,.14),0 1px 3px 0 rgba(0,0,0,.12);
 }
 
 .layout {
@@ -101,37 +78,9 @@
     flex-wrap: nowrap;
 }
 
-.card--flat {
-    box-shadow: 0 0 0 0 rgba(0,0,0,.2),0 0 0 0 rgba(0,0,0,.14),0 0 0 0 rgba(0,0,0,.12);
-}
-
-.application.theme--light a {
-    cursor: pointer;
-}
-
-.card--hover {
-    cursor: pointer;
-    transition: all .4s cubic-bezier(.25,.8,.25,1);
-        transition-property: all;
-    transition-property: box-shadow;
-}
-
-.container.grid-list-md {
-    padding: 8px;
-}
-
-.container.fluid {
-    max-width: 100%;
-}
-
-.card > :first-child:not(.btn) {
-    border-top-left-radius: inherit;
-    border-top-right-radius: inherit;
-}
-
-.card > :last-child:not(.btn) {
-    border-bottom-left-radius: inherit;
-    border-bottom-right-radius: inherit;
+.layout.row {
+    -ms-flex-direction: row;
+    flex-direction: row;
 }
 
 .layout.wrap {
@@ -139,37 +88,12 @@
     flex-wrap: wrap;
 }
 
-.layout.row {
-    -ms-flex-direction: row;
-    flex-direction: row;
+.mt-2 {
+    margin-top: 8px;
 }
 
-.container.grid-list-md .layout .flex {
-    padding: 4px;
-}
-
-.flex.lg4 {
-    -ms-flex-preferred-size: 33.33%;
-    flex-basis: 33.33%;
-    -ms-flex-positive: 0;
-    flex-grow: 0;
-    max-width: 33.33%;
-}
-
-.flex.md4 {
-    -ms-flex-preferred-size: 33.33%;
-    flex-basis: 33.33%;
-    -ms-flex-positive: 0;
-    flex-grow: 0;
-    max-width: 33.33%;
-}
-
-.flex.sm6 {
-    -ms-flex-preferred-size: 50%;
-    flex-basis: 50%;
-    -ms-flex-positive: 0;
-    flex-grow: 0;
-    max-width: 50%;
+.mb-3 {
+    margin-bottom: 16px !important;
 }
 
 .flex.xs12 {
@@ -180,26 +104,69 @@
     max-width: 100%;
 }
 
-.flex {
-    -ms-flex: 1 1 auto;
-    flex: 1 1 auto;
-        flex-grow: 1;
-        flex-basis: auto;
+.text-xs-center {
+    text-align: center !important;
 }
 
-img {
-  width: 350px;
-  height: 200px;
-}
-
-.headings {
+* {
+    background-repeat: no-repeat;
+    padding: 0;
     margin: 0;
 }
+
+.card {
+    display: block;
+    border-radius: 2px;
+    min-width: 0;
+    position: relative;
+    text-decoration: none;
+    box-shadow: 0 2px 1px -1px rgba(0,0,0,.2),0 1px 1px 0 rgba(0,0,0,.14),0 1px 3px 0 rgba(0,0,0,.12);
+}
+
+.box {
+  margin-left: 5px;
+  margin-right: 5px;
+}
+
+.flex.lg4 {
+    -ms-flex-preferred-size: 33.33333333333333%;
+    flex-basis: 33.33333333333333%;
+    -ms-flex-positive: 0;
+    flex-grow: 0;
+    max-width: 33.33333333333333%;
+}
+
+.flex.md4 {
+    -ms-flex-preferred-size: 33.33333333333333%;
+    flex-basis: 33.33333333333333%;
+    -ms-flex-positive: 0;
+    flex-grow: 0;
+    max-width: 33.33333333333333%;
+}
+
+.flex.sm6 {
+    -ms-flex-preferred-size: 50%;
+    flex-basis: auto;
+    -ms-flex-positive: 0;
+    flex-grow: 1;
+    max-width: 50%;
+    min-width: 385px;
+}
+
+.card__text {
+    padding: 16px;
+    width: 100%;
+}
+
+.ma-0 {
+    margin: 0 !important;
+}
+
 .headline {
-    font-size: 24px;
+    font-size: 24px !important;
     font-weight: 400;
-    line-height: 32px;
-    letter-spacing: normal;
+    line-height: 32px !important;
+    letter-spacing: normal !important;
 }
 
 .grey--text {
@@ -222,12 +189,26 @@ img {
     height: 100%;
 }
 
-.card__media__content {
-    display: -ms-flexbox;
-    display: flex;
-    -ms-flex: 1 1 auto;
-    flex: 1 1 auto;
-    position: relative;
+img {
+  width: 350px;
+  height: 200px;
+}
+
+@media screen and (max-width: 1059px) {
+  .application--wrap {
+    width: 900px;
+    flex-wrap: wrap;
+    background: white;
+    margin: auto;
+  }
+}
+
+@media screen and (max-width: 759px) {
+  .application--wrap {
+    width: 300px;
+    flex-wrap: wrap;
+    background: white;
+  }
 }
 
 </style>
